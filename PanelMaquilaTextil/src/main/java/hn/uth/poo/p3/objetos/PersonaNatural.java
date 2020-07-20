9dd5c55679b0487c6197c4b78f40782bd978cc07
maureenbarahona <maureenbarahona@gmail.com>
2020-07-19 22:17:25 -0600

    Creacion clase Persona Natural

---
+++ PanelMaquilaTextil/src/main/java/hn/uth/poo/p3/objetos/PersonaNatural.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.uth.poo.p3.objetos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author maureen
+ */
+public class PersonaNatural extends Persona {
+
+    private String Identidad;
+    private String PrimerNombre;
+    private String SegundoNombre;
+    private String PrimerApellido;
+    private String SegundoApellido;
+    private String Nacionalidad;
+    
+    private String Direccion;
+    private String Telefono;
+    private String Profesion;
+
+ 
+    
+
+    public PersonaNatural(String Identidad, String PrimerNombre, String SegundoNombre, String PrimerApellido, String SegundoApellido, String Nacionalidad, String Direccion, String Telefono, String Profesion, int CodPersona, String TigoPersona, String Nombre, Date FecCreacion) {
+        super(CodPersona, TigoPersona, Nombre, FecCreacion);
+        this.Identidad = Identidad;
+        this.PrimerNombre = PrimerNombre;
+        this.SegundoNombre = SegundoNombre;
+        this.PrimerApellido = PrimerApellido;
+        this.SegundoApellido = SegundoApellido;
+        this.Nacionalidad = Nacionalidad;
+        this.Direccion = Direccion;
+        this.Telefono = Telefono;
+        this.Profesion = Profesion;
+    }
+    
+    
+
+
+    public String getPrimerNombre() {
+        return PrimerNombre;
+    }
+
+    public void setPrimerNombre(String PrimerNombre) {
+        this.PrimerNombre = PrimerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return SegundoNombre;
+    }
+
+    public void setSegundoNombre(String SegundoNombre) {
+        this.SegundoNombre = SegundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return PrimerApellido;
+    }
+
+    public void setPrimerApellido(String PrimerApellido) {
+        this.PrimerApellido = PrimerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return SegundoApellido;
+    }
+
+    public void setSegundoApellido(String SegundoApellido) {
+        this.SegundoApellido = SegundoApellido;
+    }
+
+    public String getNacionalidad() {
+        return Nacionalidad;
+    }
+
+    public void setNacionalidad(String Nacionalidad) {
+        this.Nacionalidad = Nacionalidad;
+    }
+
+    public String getIdentidad() {
+        return Identidad;
+    }
+
+    public void setIdentidad(String Identidad) {
+        this.Identidad = Identidad;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getProfesion() {
+        return Profesion;
+    }
+
+    public void setProfesion(String Profesion) {
+        this.Profesion = Profesion;
+    }
+    
+
+}
