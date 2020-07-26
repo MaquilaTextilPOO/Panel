a58e26216086bb83f34b36a31f67db093ef2ed94
Mar-cybe <67813877+Mar-cybe@users.noreply.github.com>
2020-07-26 17:27:41 -0600

    Agregar campos  a la clase clientes

--- PanelMaquilaTextil/src/main/java/hn/uth/poo/p3/objetos/Clientes.java
+++ PanelMaquilaTextil/src/main/java/hn/uth/poo/p3/objetos/Clientes.java
@@ -5,10 +5,34 @@
  */
 package hn.uth.poo.p3.objetos;
 
+import java.util.Date;
+
 /**
  *
  * @author maureen
  */
 public class Clientes extends Persona {
+    private int codCliente;
+
+    public Clientes() {
+        super();
+    }
+    
+
+    public Clientes(int codCliente, int CodPersona, String TigoPersona, String Nombre, Date FecCreacion) {
+        super(CodPersona, TigoPersona, Nombre, FecCreacion);
+        this.codCliente = codCliente;
+    }
+    
+    
+
+    public int getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(int codCliente) {
+        this.codCliente = codCliente;
+    }
+    
     
 }
