c8da32e137da1e87141950d8939fa21cd6a39f1c
maureenbarahona <maureenbarahona@gmail.com>
2020-07-27 17:22:12 -0600

    Especificaciones Ficha tecnica, inventario y materiales

--- PanelMaquilaTextil/src/main/java/hn/uth/poo/p3/objetos/Inventarios.java
+++ PanelMaquilaTextil/src/main/java/hn/uth/poo/p3/objetos/Inventarios.java
@@ -10,5 +10,57 @@ package hn.uth.poo.p3.objetos;
  * @author maureen
  */
 public class Inventarios  {
+    private int CodInventario;
+    private Productos Productos;
+    private int CantidadInicial;
+    private int CantidadExistencia;
+
+    public Inventarios() {
+        super();
+    }
+    
+    
+
+    public Inventarios(int CodInventario, Productos Productos, int CantidadInicial, int CantidadExistencia) {
+        this.CodInventario = CodInventario;
+        this.Productos = Productos;
+        this.CantidadInicial = CantidadInicial;
+        this.CantidadExistencia = CantidadExistencia;
+    }
+    
+    
+
+    public int getCodInventario() {
+        return CodInventario;
+    }
+
+    public void setCodInventario(int CodInventario) {
+        this.CodInventario = CodInventario;
+    }
+
+    public Productos getProductos() {
+        return Productos;
+    }
+
+    public void setProductos(Productos Productos) {
+        this.Productos = Productos;
+    }
+
+    public int getCantidadInicial() {
+        return CantidadInicial;
+    }
+
+    public void setCantidadInicial(int CantidadInicial) {
+        this.CantidadInicial = CantidadInicial;
+    }
+
+    public int getCantidadExistencia() {
+        return CantidadExistencia;
+    }
+
+    public void setCantidadExistencia(int CantidadExistencia) {
+        this.CantidadExistencia = CantidadExistencia;
+    }
+    
     
 }
