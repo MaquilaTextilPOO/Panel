f7250a7847fe45374596c549c6131f610e39f26e
maureenbarahona <maureenbarahona@gmail.com>
2020-07-26 18:27:36 -0600

    Esoecificacion clase Proveedores

--- PanelMaquilaTextil/src/main/java/hn/uth/poo/p3/objetos/Proveedores.java
+++ PanelMaquilaTextil/src/main/java/hn/uth/poo/p3/objetos/Proveedores.java
@@ -5,10 +5,45 @@
  */
 package hn.uth.poo.p3.objetos;
 
+import java.util.Date;
+
 /**
  *
  * @author maureen
  */
 public class Proveedores extends Persona{
+    private int CodProveedor;
+    private String TipoProveedor;
+
+    public Proveedores() {
+        super();
+    }
+    
+    
+
+    public Proveedores(int CodProveedor, String TipoProveedor, int CodPersona, String TigoPersona, String Nombre, Date FecCreacion) {
+        super(CodPersona, TigoPersona, Nombre, FecCreacion);
+        this.CodProveedor = CodProveedor;
+        this.TipoProveedor = TipoProveedor;
+    }
+    
+    
+
+    public int getCodProveedor() {
+        return CodProveedor;
+    }
+
+    public void setCodProveedor(int CodProveedor) {
+        this.CodProveedor = CodProveedor;
+    }
+
+    public String getTipoProveedor() {
+        return TipoProveedor;
+    }
+
+    public void setTipoProveedor(String TipoProveedor) {
+        this.TipoProveedor = TipoProveedor;
+    }
+    
     
 }
