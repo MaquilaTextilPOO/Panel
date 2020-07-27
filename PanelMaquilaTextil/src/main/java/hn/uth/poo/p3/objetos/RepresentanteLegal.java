0f15f7a5989c7554b67858656427400b43f7fd03
maureenbarahona <maureenbarahona@gmail.com>
2020-07-26 18:24:04 -0600

    Correccion de herencia de representante Legal

--- PanelMaquilaTextil/src/main/java/hn/uth/poo/p3/objetos/RepresentanteLegal.java
+++ PanelMaquilaTextil/src/main/java/hn/uth/poo/p3/objetos/RepresentanteLegal.java
@@ -11,21 +11,19 @@ import java.util.Date;
  *
  * @author maureen
  */
-public class RepresentanteLegal extends Persona {
+public class RepresentanteLegal extends PersonaJuridica {
     private boolean indRepresentante;
 
     public RepresentanteLegal() {
         super();
     }
-    
-    
 
-    public RepresentanteLegal(boolean indRepresentante, int CodPersona, String TigoPersona, String Nombre, Date FecCreacion) {
-        super(CodPersona, TigoPersona, Nombre, FecCreacion);
+    public RepresentanteLegal(boolean indRepresentante, int RTN, String RazonSocial, String NombreComercial, String Giro, String DireccionComercial, hn.uth.poo.p3.objetos.Ciudad Ciudad, String Email, String TelefonoPrincipal, String RepresentanteL, String IDRepresentanteL, int CodPersona, String TigoPersona, String Nombre, Date FecCreacion) {
+        super(RTN, RazonSocial, NombreComercial, Giro, DireccionComercial, Ciudad, Email, TelefonoPrincipal, RepresentanteL, IDRepresentanteL, CodPersona, TigoPersona, Nombre, FecCreacion);
         this.indRepresentante = indRepresentante;
     }
-
     
+        
     public boolean isIndRepresentante() {
         return indRepresentante;
     }
