64d617637972e8289614dba07071e0df4dc0a011
Mar-cybe <67813877+Mar-cybe@users.noreply.github.com>
2020-08-07 20:57:19 -0600

    Creación de pnlNorma dentro de el Package gui. Cambio de nombre de Package de Tintura a tintura. Cambio de nombre de Package de Proveedores a proveedores.

--- PanelMaquilaTextil/src/main/java/hn/uth/poo/p3/presentacion/gui/pnlNorma.java
+++ PanelMaquilaTextil/src/main/java/hn/uth/poo/p3/presentacion/gui/pnlNorma.java
@@ -5,12 +5,10 @@
  */
 package hn.uth.poo.p3.presentacion.gui;
 
-import hn.uth.poo.p3.negocio.tintura.TinturaNegocio;
-import hn.uth.poo.p3.recursos.clases.Tintura;
+import hn.uth.poo.p3.negocio.norma.NormaNegocio;
+import hn.uth.poo.p3.recursos.clases.Norma;
 import java.awt.HeadlessException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
@@ -18,31 +16,30 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author MARLON HERNANDEZ
  */
-public class pnlTintura extends javax.swing.JPanel {
+public class pnlNorma extends javax.swing.JPanel {
     
     DefaultTableModel modelo;
-
     
     /**
-     * Creates new form pnlTintura
+     * Creates new form pnlNorma
      */
-    public pnlTintura() {
+    public pnlNorma() {
         initComponents();
         inicio();
         Leer();
     }
     
     private void inicio() {
-        txtCodTintura.setText("0");
+        txtCodNorma.setText("0");
         modelo = (DefaultTableModel) tblDatos.getModel();
     }
-    
+
     private void Leer() {
         try {
-            List<Tintura> listaTintura = new TinturaNegocio().Leer();
+            List<Norma> listaNorma = new NormaNegocio().Leer();
             modelo.setRowCount(0);
-            for (Tintura tintura : listaTintura) {
-                Object[] registroLeido = {tintura.getCodTintura(), tintura.getDesTintura()};
+            for (Norma normae : listaNorma) {
+                Object[] registroLeido = {normae.getCodNorma(), normae.getDescripcion()};
                 modelo.addRow(registroLeido);
 
             }
@@ -62,38 +59,29 @@ public class pnlTintura extends javax.swing.JPanel {
     private void initComponents() {
 
         lblTitulo = new javax.swing.JLabel();
-        lblCodTintura = new javax.swing.JLabel();
-        txtCodTintura = new javax.swing.JTextField();
-        lblDesTintura = new javax.swing.JLabel();
-        txtDesTintura = new javax.swing.JTextField();
-        btnBuscar = new javax.swing.JButton();
+        lblCodNorma = new javax.swing.JLabel();
+        txtCodNorma = new javax.swing.JTextField();
+        lblDescripcion = new javax.swing.JLabel();
+        txtDescripcion = new javax.swing.JTextField();
         btnGuardar = new javax.swing.JButton();
         btnActualizar = new javax.swing.JButton();
         btnEliminar = new javax.swing.JButton();
         btnLeer = new javax.swing.JButton();
+        btnBuscar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblDatos = new javax.swing.JTable();
 
         lblTitulo.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         lblTitulo.setForeground(new java.awt.Color(48, 34, 84));
-        lblTitulo.setText("Mantinimiento Tintura");
+        lblTitulo.setText("Mantenimiento Norma");
 
-        lblCodTintura.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        lblCodTintura.setForeground(new java.awt.Color(48, 34, 84));
-        lblCodTintura.setText("Codigo:");
+        lblCodNorma.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        lblCodNorma.setForeground(new java.awt.Color(48, 34, 84));
+        lblCodNorma.setText("Codigo:");
 
-        lblDesTintura.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        lblDesTintura.setForeground(new java.awt.Color(48, 34, 84));
-        lblDesTintura.setText("Descripcion:");
-
-        btnBuscar.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        btnBuscar.setForeground(new java.awt.Color(48, 34, 84));
-        btnBuscar.setText("Buscar");
-        btnBuscar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnBuscarActionPerformed(evt);
-            }
-        });
+        lblDescripcion.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        lblDescripcion.setForeground(new java.awt.Color(48, 34, 84));
+        lblDescripcion.setText("Descripcion:");
 
         btnGuardar.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         btnGuardar.setForeground(new java.awt.Color(48, 34, 84));
@@ -131,6 +119,15 @@ public class pnlTintura extends javax.swing.JPanel {
             }
         });
 
+        btnBuscar.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        btnBuscar.setForeground(new java.awt.Color(48, 34, 84));
+        btnBuscar.setText("Buscar");
+        btnBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBuscarActionPerformed(evt);
+            }
+        });
+
         tblDatos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
@@ -140,7 +137,7 @@ public class pnlTintura extends javax.swing.JPanel {
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class
+                java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
                 false, false
@@ -166,121 +163,118 @@ public class pnlTintura extends javax.swing.JPanel {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(203, 203, 203)
+                .addComponent(lblTitulo)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(91, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(lblDescripcion)
+                            .addComponent(lblCodNorma))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtDescripcion, javax.swing.GroupLayout.DEFAULT_SIZE, 127, Short.MAX_VALUE)
+                            .addComponent(txtCodNorma))
+                        .addGap(18, 18, 18)
+                        .addComponent(btnBuscar)
+                        .addGap(154, 154, 154))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(btnGuardar)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(btnActualizar)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(btnEliminar)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(btnLeer))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(lblDesTintura)
-                                    .addComponent(lblCodTintura))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(txtDesTintura, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(txtCodTintura, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(lblTitulo, javax.swing.GroupLayout.Alignment.TRAILING))
                                 .addGap(18, 18, 18)
-                                .addComponent(btnBuscar))))
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addGap(128, 128, 128)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 281, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(135, Short.MAX_VALUE))
+                                .addComponent(btnActualizar)
+                                .addGap(18, 18, 18)
+                                .addComponent(btnEliminar)
+                                .addGap(18, 18, 18)
+                                .addComponent(btnLeer)))
+                        .addGap(132, 132, 132))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lblTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addGap(21, 21, 21)
+                .addComponent(lblTitulo)
+                .addGap(33, 33, 33)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblCodTintura)
-                    .addComponent(txtCodTintura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                    .addComponent(lblCodNorma)
+                    .addComponent(txtCodNorma, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblDesTintura)
-                    .addComponent(txtDesTintura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblDescripcion)
+                    .addComponent(txtDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnBuscar))
-                .addGap(18, 18, 18)
+                .addGap(24, 24, 24)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnGuardar)
                     .addComponent(btnActualizar)
                     .addComponent(btnEliminar)
                     .addComponent(btnLeer))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(55, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(93, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
         // TODO add your handling code here:
         try {
-            Tintura tintura = new Tintura();
-            tintura.setCodTintura(Integer.parseInt(txtCodTintura.getText())+1);
-            tintura.setDesTintura(txtDesTintura.getText());
-            String respuesta = new TinturaNegocio().Insertar(tintura);
+            Norma norma = new Norma();
+            norma.setCodNorma(txtCodNorma.getText()+1);
+            norma.setDescripcion(txtDescripcion.getText());
+            String respuesta = new NormaNegocio().Insertar(norma);
             JOptionPane.showMessageDialog(null, "Guardado", "Exito", JOptionPane.INFORMATION_MESSAGE);
             Leer();
         } catch (HeadlessException | NumberFormatException e) {
             JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-        } catch (Exception ex) {
-            Logger.getLogger(pnlTintura.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_btnGuardarActionPerformed
-
-    private void btnActualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnActualizarActionPerformed
-        // TODO add your handling code here:
-        try {
-            Tintura tintura = new Tintura();
-            tintura.setCodTintura(Integer.parseInt(txtCodTintura.getText()));
-            tintura.setDesTintura(txtDesTintura.getText());
-            new TinturaNegocio().Actualizar(tintura);
-            JOptionPane.showMessageDialog(null, "Actualizado", "Exito", JOptionPane.INFORMATION_MESSAGE);
-            Leer();
-        } catch (HeadlessException | NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-        } catch (Exception ex) {
-            Logger.getLogger(pnlTintura.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_btnActualizarActionPerformed
-
-    private void btnEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEliminarActionPerformed
-        // TODO add your handling code here:
-        try {
-            Tintura tintura = new Tintura();
-            tintura.setCodTintura(Integer.parseInt(txtCodTintura.getText()));
-            tintura.setDesTintura(txtDesTintura.getText());
-            new TinturaNegocio().Eliminar(tintura);
-            JOptionPane.showMessageDialog(null, "Eliminado", "Exito", JOptionPane.INFORMATION_MESSAGE);
-            Leer();
-        } catch (HeadlessException | NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-        } catch (Exception ex) {
-            Logger.getLogger(pnlTintura.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_btnEliminarActionPerformed
 
     private void btnLeerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLeerActionPerformed
         // TODO add your handling code here:
         Leer();
     }//GEN-LAST:event_btnLeerActionPerformed
 
+    private void btnActualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnActualizarActionPerformed
+        // TODO add your handling code here:
+        try {
+            Norma norma = new Norma();
+            norma.setCodNorma(txtCodNorma.getText());
+            norma.setDescripcion(txtDescripcion.getText());
+            new NormaNegocio().Actualizar(norma);
+            JOptionPane.showMessageDialog(null, "Actualizado", "Exito", JOptionPane.INFORMATION_MESSAGE);
+            Leer();
+        } catch (HeadlessException | NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }//GEN-LAST:event_btnActualizarActionPerformed
+
+    private void btnEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEliminarActionPerformed
+        // TODO add your handling code here:
+        try {
+            Norma norma = new Norma();
+            norma.setCodNorma(txtCodNorma.getText());
+            norma.setDescripcion(txtDescripcion.getText());
+            new NormaNegocio().Eliminar(norma);
+            JOptionPane.showMessageDialog(null, "Eliminado", "Exito", JOptionPane.INFORMATION_MESSAGE);
+            Leer();
+        } catch (HeadlessException | NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }//GEN-LAST:event_btnEliminarActionPerformed
+
     private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
         // TODO add your handling code here:
-        try {Tintura tintura = new Tintura();
-            tintura.setDesTintura(txtDesTintura.getText());
-            List<Tintura> listaTintura = new TinturaNegocio().Buscar(tintura);
+        try {
+            Norma norma = new Norma();
+            norma.setDescripcion(txtDescripcion.getText());
+            List<Norma> listaNorma = new NormaNegocio().Buscar(norma);
             modelo.setRowCount(0);
-            for (Tintura tintura1 : listaTintura) {
-                Object[] registroLeido = {tintura1.getCodTintura(), tintura1.getDesTintura()};
+            for (Norma norma1 : listaNorma) {
+                Object[] registroLeido = {norma1.getCodNorma(), norma1.getDescripcion()};
                 modelo.addRow(registroLeido);
             }
             tblDatos.setModel(modelo);
@@ -292,8 +286,9 @@ public class pnlTintura extends javax.swing.JPanel {
     private void tblDatosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblDatosMouseClicked
         // TODO add your handling code here:
         int filaseleccionada = tblDatos.getSelectedRow();
-        txtCodTintura.setText(modelo.getValueAt(filaseleccionada, 0).toString());
-        txtDesTintura.setText(modelo.getValueAt(filaseleccionada, 1).toString());
+        txtCodNorma.setText(modelo.getValueAt(filaseleccionada, 0).toString());
+        txtDescripcion.setText(modelo.getValueAt(filaseleccionada, 1).toString());
+        
     }//GEN-LAST:event_tblDatosMouseClicked
 
 
@@ -304,11 +299,11 @@ public class pnlTintura extends javax.swing.JPanel {
     private javax.swing.JButton btnGuardar;
     private javax.swing.JButton btnLeer;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JLabel lblCodTintura;
-    private javax.swing.JLabel lblDesTintura;
+    private javax.swing.JLabel lblCodNorma;
+    private javax.swing.JLabel lblDescripcion;
     private javax.swing.JLabel lblTitulo;
     private javax.swing.JTable tblDatos;
-    private javax.swing.JTextField txtCodTintura;
-    private javax.swing.JTextField txtDesTintura;
+    private javax.swing.JTextField txtCodNorma;
+    private javax.swing.JTextField txtDescripcion;
     // End of variables declaration//GEN-END:variables
 }
